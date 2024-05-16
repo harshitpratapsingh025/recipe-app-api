3f928abb81a0909c56dae8d2bf4e81f01bcb2a21
Harshit singh <harshitpratapsingh025@gmail.com>
2024-05-16 09:25:22 +0530

    Added user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,27 @@
-from django.db import models  # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """Manage for users."""
+
+    def create_user(self, email, password=None, **extra_field):
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
